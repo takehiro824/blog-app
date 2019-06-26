052b4051851cc26a41915da90408f7613af335fb
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-06-26 15:43:18 +0900

    edit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,32 +1,43 @@
 class BlogsController < ApplicationController
-  
+ before_action :authenticate_user!, except: :index
+
   def index
+   @blogs = Blog.all.includes(:user)
   end
 
   def new
     @blog = Blog.new
   end
 
-  def create(blog_paramas)
+  def create
+    Blog.create(blog_params)
     redirect_to root_path
   end
 
   def edit
+    @blogman = Blog.find(params[:id])
   end
 
-  def update(blog_paramas)
+  def update
+    @blogman = Blog.find(params[:id])
+    if @blogman.user_id == current_user.id
+     @blogman.update(blog_params)
+    end
     redirect_to root_path
   end
 
-  def  destory
+  def destroy
+    @blogman = Blog.find(params[:id])
+    if @blogman.user_id == current_user.id
+      @blogman.destroy
+    redirect_to root_path
+    end
   end
 
   private
-  def blog_paramas
-    params.require(:blog).permit(:body).merge(:user)
+  def blog_params
+    params.require(:blog).permit(:body).merge(user_id: current_user.id)
   end
 
-  def set_blog
-    @blog = Blog.find(:params[:id])
-  end
+  
 end
